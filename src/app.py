615fc2e10bcfd80629f3326d5f6ea2b39395efb4
sunrabbit123 <qudwls185@naver.com>
2021-07-02 14:51:13 +0900

    ADD: comment

--- src/app.py
+++ src/app.py
@@ -7,21 +7,24 @@ from misc import HTMLGetter, Config, LoginForm
 
 app = FastAPI()
 
+# 로그인 시에 API 서버와 연동해서 로그인 수행 후 토큰 발급하는 엔드포인트
 @app.post('/login')
 async def login(body : LoginForm):
+    # 값 받아오기
     email = body.email
     pw = body.pw
 
+    # API 연동
     data = await HTMLGetter(Config.API_SERVER).set_data(email = email, pw = pw).get_json()
-    data = {"pw" : pw, "email" : email}
-    encoded_data = jwt.encode(data, Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHEM)
-    redirect_url = body.redirect_url
+    data = {"pw" : pw, "email" : email} # API 서버 있을시에 해당 부분 주석 필요
+    encoded_data = jwt.encode(data, Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHEM) # 토큰화
+    redirect_url = body.redirect_url# 리다이렉트 url 지정
 
     # return body setting
     body.email = ""
     body.pw = ""
-
     body.token = encoded_data
 
-    return RedirectResponse(redirect_url, headers={"Authorization" : encoded_data}, status_code=307)
+    # 307코드로 리다이렉트
+    return RedirectResponse(redirect_url, status_code=307)
 
